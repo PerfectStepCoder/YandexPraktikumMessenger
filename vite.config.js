cf24d76108891f37be74007b9cf8fce6db57b54b
CreatorDmitrij <codisculptor@gmail.com>
2025-05-30 10:37:23 +0200

    Deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,25 @@
 import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "src",
   plugins: [
-    handlebars({
-      partialDirectory: "./src/pages/oldTemplates",
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "_redirects",
-          dest: "",
-        },
-        {
-          src: "./pages/oldTemplates/*.hbs",
-          dest: "assets/pages",
-        },
-      ],
-    }),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "_redirects",
+    //       dest: "",
+    //     },
+    //   ],
+    // }),
   ],
   server: {
     port: 3000,
   },
-  assetsInclude: ["./pages/oldTemplates/*.hbs"],
   build: {
     outDir: "../build",
-    rollupOptions: {
-      external: ["uuid"], // Указываем, что модуль должен оставаться внешним
-    },
+    // rollupOptions: {
+    //   external: ["uuid"], // Указываем, что модуль должен оставаться внешним
+    // },
   },
 });
