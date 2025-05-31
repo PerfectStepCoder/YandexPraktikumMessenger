a526f1375e80cfd7e497c73f898962c44e1cc4db
CreatorDmitrij <codisculptor@gmail.com>
2025-05-31 18:46:38 +0200

    Fix

--- src/pages/profile/Profile.ts
+++ src/pages/profile/Profile.ts
@@ -2,8 +2,6 @@ import { FormBlock, FormProps } from "../../common/FormBlock";
 import template from "./template";
 import "./Profile.css";
 
-const apiUrl = import.meta.env.VITE_API_URL;
-
 export default class Profile extends FormBlock {
   constructor(props: FormProps) {
     super(props, template);
